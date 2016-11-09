97e65eb427f40e7a6625eac871d1e2314b529740
DucVu1908 <nvv190894@gmail.com>
2016-11-09 09:26:34 +0700

    Update Program.cs

--- BaiToanBOM/BaiToanBOM/Program.cs
+++ BaiToanBOM/BaiToanBOM/Program.cs
@@ -36,19 +36,7 @@ namespace BaiToanBOM
             }
 
             Console.WriteLine();
-
-            Console.WriteLine("Cac tri trong myStack:\t");
-            InGiaTri(stack);
-            Console.WriteLine("\nDa day ra\t{0}", stack.Pop());
-            Console.WriteLine("Cac gia tri trong myStack:\t");
-            InGiaTri(stack);
-            Console.WriteLine("\nDa day ra\t{0}", stack.Pop());
-            Console.WriteLine("Cac gia tri trong myStack:\t");
-            InGiaTri(stack);
-            Console.WriteLine("Peek:\t{0}", stack.Peek());
-            Console.WriteLine("Cac gia tri trong myStack:\t");
-            InGiaTri(stack);
-            Console.ReadLine();
+            Console.Realine();
         }
     }
 }
